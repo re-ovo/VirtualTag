395512dab744c7ffc3471246b642438717f5b704
re-ovo <qq1609403959@outlook.com>
2021-11-19 19:45:49 +0800

    fix: maybe fixed the tag disappeared issue?

--- src/main/kotlin/me/rerere/virtualtag/configuration/files/MainConfig.kt
+++ src/main/kotlin/me/rerere/virtualtag/configuration/files/MainConfig.kt
@@ -4,7 +4,7 @@ import me.rerere.virtualtag.configuration.ConfigFile
 import me.rerere.virtualtag.tag.TagGroup
 
 class MainConfig : ConfigFile("config.yml") {
-    val updateInterval = int("general.update_interval", 20)
+    val updateInterval = int("general.update_interval", 20).coerceAtLeast(1)
     val multipleNameTags = boolean("general.multiple_name_tags", false)
     val groups = configuration.getConfigurationSection("groups")?.let { section ->
         section.getKeys(false).map {
--- src/main/kotlin/me/rerere/virtualtag/tag/VirtualTagManager.kt
+++ src/main/kotlin/me/rerere/virtualtag/tag/VirtualTagManager.kt
@@ -10,7 +10,13 @@ import org.bukkit.entity.Player
 import java.util.*
 
 class VirtualTagManager {
+
+    // Cache the tag when the player uploads the update to prevent frequent tag updates
     private val previousTagCache = hashMapOf<UUID, Tag>()
+
+    // Ensure that the tag can be updated every certain time even though it has not changed
+    private val noUpdateTicks = hashMapOf<UUID, Int>()
+
     val task = timerTask(
         interval = virtualTag().configModule.mainConfig.updateInterval.toLong()
     ) {
@@ -47,13 +53,18 @@ class VirtualTagManager {
         val previousTag = previousTagCache[player.uniqueId]
 
         // Name tag changed, require update
-        if (targetTag != previousTag) {
+        if (targetTag != previousTag || (noUpdateTicks[player.uniqueId] ?: 0) > 100) {
             previousTagCache[player.uniqueId] = targetTag
+            noUpdateTicks[player.uniqueId] = 0
+
             virtualTag().tagHandler.setPlayerTag(player, targetTag)
+        } else {
+            noUpdateTicks[player.uniqueId] = noUpdateTicks[player.uniqueId]?.plus(1) ?: 1
         }
     }
 
     fun playerQuit(player: Player) {
-        previousTagCache.remove(player.uniqueId)
+        previousTagCache -= player.uniqueId
+        noUpdateTicks -= player.uniqueId
     }
 }
