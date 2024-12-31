efd851f20ce75a1f59f5cc7c1577f27ff7dbb188
Loyisa <loyisa893@gmail.com>
2024-12-31 11:03:40 +0800

    fix: we already have allPlayers

--- src/main/kotlin/me/rerere/virtualtag/util/PacketEventsUtil.kt
+++ src/main/kotlin/me/rerere/virtualtag/util/PacketEventsUtil.kt
@@ -2,11 +2,9 @@ package me.rerere.virtualtag.util
 
 import com.github.retrooper.packetevents.PacketEvents
 import com.github.retrooper.packetevents.wrapper.PacketWrapper
-import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
-fun broadcastPacket(packet: PacketWrapper<*>) =
-    Bukkit.getOnlinePlayers().forEach { player -> player.sendPacket(packet) }
+fun broadcastPacket(packet: PacketWrapper<*>) = allPlayers { player -> player.sendPacket(packet) }
 
 fun Player.sendPacket(packet: PacketWrapper<*>) = PacketEvents.getAPI().playerManager.sendPacket(this, packet)
 
